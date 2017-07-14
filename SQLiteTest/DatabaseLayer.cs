988a548c9e6061d233b66e544bc5d85897ef8626
Sinka-Pálinkás Gabriella <fusewent@gmail.com>
2017-07-14 12:59:05 +0200

    can't protect db with pw, just encrypt

---
+++ SQLiteTest/DatabaseLayer.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SQLite;
+using System.IO;
+
+namespace SQLiteTest
+{
+    class DatabaseLayer
+    {
+        public void CreateDatabaseWPassword()
+        {
+            // ez tk nem is szükséges, kapcsolódásnál létrehozza, ha korábban nem létezett (lehet .db is, txt, akármi)
+            SQLiteConnection.CreateFile("d:/MyDatabase.sqlite");
+            SQLiteConnection _dbConnection;
+            _dbConnection = new SQLiteConnection("Data Source=d:/MyDatabase.sqlite;Version=3;");
+            // ezt a pw-t magasról lefossa
+            _dbConnection.SetPassword("testpw");
+
+            // _dbConnection.ChangePassword("new_password");
+            // _dbConnection.ChangePassword(String.Empty); reset or remove
+        }
+
+        public SQLiteConnection ConnectionOpener()
+        {
+            try
+            {
+                SQLiteConnection _dbConnection;
+                // ezt a pw-t fogja használni kódolásnál, querynél
+                _dbConnection = new SQLiteConnection("Data Source=d:/MyDatabase.sqlite;Version=3;Password=testpw;");
+                //_dbConnection = new SQLiteConnection("Data Source=d:/MyDatabase.sqlite;Version=3;");
+                _dbConnection.Open();
+                return _dbConnection;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public bool CreateTable()
+        {
+            try
+            {
+                using (SQLiteConnection _dbConnection = ConnectionOpener())
+                {
+                    string sql = "create table test (blabla varchar(25))";
+                    SQLiteCommand command = new SQLiteCommand(sql, _dbConnection);
+
+                    // returns the number of rows that have been modified - for UPDATE queries
+                    command.ExecuteNonQuery();
+                }
+                return true;
+            }
+            catch (Exception)
+            {
+
+            }
+            return false;
+        }
+
+        public bool InsertData(string randomText)
+        {
+            try
+            {
+                using (SQLiteConnection _dbConnection = ConnectionOpener())
+                {
+                    string sql = "insert into test (blabla) values ('" + randomText + "')"; 
+                    SQLiteCommand command = new SQLiteCommand(sql, _dbConnection);
+
+                    // returns the number of rows that have been modified - for UPDATE queries
+                    command.ExecuteNonQuery();
+                }
+                return true;
+            }
+            catch (Exception)
+            {
+
+            }
+            return false;
+        }
+
+        public bool QueryDatabase()
+        {
+            try
+            {
+                using (SQLiteConnection _dbConnection = ConnectionOpener())
+                {
+                    string sql = "select * from test";
+                    SQLiteCommand command = new SQLiteCommand(sql, _dbConnection);
+
+                    SQLiteDataReader reader = command.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        Console.WriteLine(reader["blabla"]);
+                    }
+                }
+                return true;
+            }
+            catch (Exception)
+            {
+
+            }
+            return false;
+        }
+    }
+}
--- SQLiteTest/Program.cs
+++ SQLiteTest/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Data.SQLite;
 
 namespace SQLiteTest
 {
@@ -9,6 +10,42 @@ namespace SQLiteTest
     {
         static void Main(string[] args)
         {
+            DatabaseLayer dl = new DatabaseLayer();
+            int menupontSzama;
+
+            Console.WriteLine("1. Adatbazisfajl letrehozasa admin userrel\n");
+            Console.WriteLine("2. Tabla letrehozasa\n");
+            Console.WriteLine("3. Adatbevitel\n");
+            Console.WriteLine("4. Lekerdezes\n");
+            Console.WriteLine("9. Exit");
+            Console.WriteLine("***************************************");
+
+            do
+            {
+                menupontSzama = int.Parse(Console.ReadLine());
+                switch (menupontSzama)
+                {
+                    case 1:
+                        // kódból megadott pw
+                        dl.CreateDatabaseWPassword();
+                        break;
+                    case 2:
+                        // kódból megadott tábla
+                        dl.CreateTable();
+                        break;
+                    case 3:
+                        Console.WriteLine("Adat:");
+                        string adat = Console.ReadLine();
+                        dl.InsertData(adat);
+                        break;
+                    case 4:
+                        // select * table
+                        dl.QueryDatabase();
+                        break;
+                    default:
+                        break;
+                }
+            } while (menupontSzama != 9);
         }
     }
 }
